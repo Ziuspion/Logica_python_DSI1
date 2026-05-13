ee4bc8a12dcfb9a5795fd7f7486d3463e7b92b56
Orlando Júnior Brito Da Silva <orlandojuniorbritodasilva@gmail.com>
2026-05-13 16:21:40 -0300

    Agora Vai Born Again Season 3 Murdock's live again part 2 in 3D

---
+++ 1.1-exercicio01.py
@@ -0,0 +1,76 @@
+# EX1
+# Um aluno tem 10 anos. Armazene essa idade em uma variável
+# e exiba seu tipo.
+idade = 10
+print("Idade:", idade)
+print("Tipo:", type(idade))
+print("\n")
+
+# EX2
+# A temperatura medida é 23.5°C.
+# Armazene esse valor e mostre seu tipo.
+clima = 23.5
+print("Temperatura:", clima)
+print("Tipo:", type(clima))
+print("\n")
+
+# EX3
+# Crie um número complexo representando uma impedância elétrica
+# de 5 + 8j e mostre sua parte real.
+eletrica = 5 + 8j
+print("impedancia:", eletrica)
+print("Tipo:", type(eletrica.real))
+print("\n")
+
+# EX4
+# Mostre a parte imaginária do número complexo
+# criado no exercício anterior.
+eletrica = 5 + 8j
+print("impedancia:", eletrica)
+print("Tipo:", type(eletrica.imag))
+print("\n")
+
+# EX5
+# Declare uma variável chamada "populacao"
+# com o valor 8_000_000_000 (8 bilhões)
+# e mostre seu tipo.
+populacao = 8_000_000_000
+print("Total:", populacao)
+print("Tipo:", type(populacao))
+print("\n")
+
+# EX6
+# Verifique se o número 7 é do tipo int
+# usando a função type().
+num = 7
+print("Numero:", num)
+print("Tipo:", type(num))
+print("\n")
+
+# EX7
+# Crie uma variável chamada "aprovado"
+# com o valor booleano True e mostre seu tipo.
+aprovado = True
+print("Valor:", True)
+print ("Tipo:", type(aprovado))
+print("\n")
+
+# EX8
+# Some True e False e mostre o resultado
+# e também o tipo do resultado.
+Total = True + False
+print("Resultado:", Total)
+print("Tipo:", type(Total))
+print("\n")
+
+# EX9
+# Pesquise e mostre qual é o valor máximo
+# que um número inteiro pode ter em Python.
+print("Em Python 3, o tipo de dado int não tem um valor máximo fixo. Os números inteiros possuem precisão arbitrária e podem crescer até o limite da memória RAM disponível no seu sistema")
+print("\n")
+
+# EX10
+# Mostre a representação em binário
+# do número 10 usando uma função do Python.
+print("\nRepresentação: ", bin(10))
+print("\n")
